1f3be52df17a90fe6a7c1b2a47434d8248cadcec
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-06-20 18:58:33 +0500

    end of js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// var increment = document.getElementById("increment");
+// var decrement = document.getElementById("decrement");
+var display = document.getElementById("display");
+var container = document.getElementById("container");
+var couterNumber = document.getElementById("couterNumber");
+var buttons = document.getElementById("buttons");
+
+var num = 0;
+
+function increment(elelment){
+    num++
+    couterNumber.innerHTML = num
+
+};
+
+function decrement(elelment){
+    num--
+    couterNumber.innerHTML = num 
+    
+};
 
